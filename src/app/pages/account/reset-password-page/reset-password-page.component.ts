371d23e60e1ff71eb13c1dcfa9678432c4168e11
pedrofbaltar <pfbaltariead@gmail.com>
2022-08-20 13:50:19 -0300

    Adicionado página de recuperação de senha

---
+++ src/app/pages/account/reset-password-page/reset-password-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password-page',
+  templateUrl: './reset-password-page.component.html',
+})
+export class ResetPasswordPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
