cf0b231a7c54036b5f5211f9eb345d092391d88d
pedrofbaltar <pfbaltariead@gmail.com>
2022-08-20 13:49:43 -0300

    Adicionado carrinho

---
+++ src/app/pages/store/cart-page/cart-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-page',
+  templateUrl: './cart-page.component.html',
+})
+export class CartPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
