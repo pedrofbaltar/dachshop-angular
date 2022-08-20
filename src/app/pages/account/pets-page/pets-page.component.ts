598123744f533f122f13d4fb3c2e78cd299f7727
pedrofbaltar <pfbaltariead@gmail.com>
2022-08-20 13:50:29 -0300

    Adicionado página listagem de pets

---
+++ src/app/pages/account/pets-page/pets-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pets-page',
+  templateUrl: './pets-page.component.html',
+})
+export class PetsPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
