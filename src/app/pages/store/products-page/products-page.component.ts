272bce1ddee4dba390bc551a717a41b49b0891ac
pedrofbaltar <pfbaltariead@gmail.com>
2022-08-20 13:45:06 -0300

    Adicionado produtos

---
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-page',
+  templateUrl: './products-page.component.html',
+})
+export class ProductsPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
