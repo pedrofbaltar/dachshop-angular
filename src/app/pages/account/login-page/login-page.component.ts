5921bbdc178dc8bbb9a42118c9ef923576b335f7
pedrofbaltar <pfbaltariead@gmail.com>
2022-08-20 13:50:44 -0300

    Adicionado página de login

---
+++ src/app/pages/account/login-page/login-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+})
+export class LoginPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
