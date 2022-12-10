d9b2a520b3f81b07e7c924891030b65e73f20035
pedrofbaltar <pfbaltariead@gmail.com>
2022-12-10 00:49:07 -0300

    Ajustado products-page

--- src/app/pages/store/products-page/products-page.component.ts
+++ src/app/pages/store/products-page/products-page.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
 })
 export class ProductsPageComponent implements OnInit {
-  constructor() {}
+  public products$: Observable<any[]>;
 
-  ngOnInit(): void {}
+  constructor(private data: DataService) {}
+
+  ngOnInit() {
+    this.products$ = this.data.getProducts();
+  }
 }
